4a8e5c15581fc7e0ca2d554671929e4c875926cf
tyler alves <tyler.alves@appfolio.com>
2019-06-24 17:04:57 -0700

    WIP - filter for circular references

--- src/createDiffPatcher.js
+++ src/createDiffPatcher.js
@@ -6,21 +6,39 @@ const defaultObjectHash = (o, idx) =>
   o && (o._id ||o._id === 0) && `$$_id:${JSON.stringify(o._id)}` ||
   '$$index:' + idx;
 
-const defaultPropertyFilter = (name, context) =>
-  typeof context.left[name] !== 'function' &&
-  typeof context.right[name] !== 'function';
+const isSeen = (name, context, seenProps) => {
+  const nameNodes = seenProps[name];
+  if (nameNodes && nameNodes.length) {
+    const matchIndex = nameNodes.findIndex(
+      item => item.left === context.left && item.right === context.right
+    );
+    if (matchIndex !== -1) {
+      return true;
+    } else {
+      nameNodes.push({ left: context.left, right: context.right });
+    }
+    return false;
+  }
+  seenProps[name] = [{ left: context.left, right: context.right }];
+};
 
-const defaultDiffPatcher = new DiffPatcher({
+const defaultPropertyFilter = (name, context, seenProps) =>
+  typeof context.left[name] !== 'function' &&
+  typeof context.right[name] !== 'function' &&
+  !isSeen(name, context, seenProps);
+
+const defaultDiffPatcher = (seenProps) => new DiffPatcher({
   arrays: { detectMove: false },
   objectHash: defaultObjectHash,
-  propertyFilter: defaultPropertyFilter
+  propertyFilter: (name, context) => defaultPropertyFilter(name, context, seenProps)
 });
 
 export default function createDiffPatcher(objectHash, propertyFilter) {
-  if (!objectHash && !propertyFilter) {
-    return defaultDiffPatcher;
-  }
+  const seenProps = {};
 
+  if (!objectHash && !propertyFilter) {
+    return defaultDiffPatcher(seenProps);
+  }
   return new DiffPatcher({
     arrays: { detectMove: false },
     objectHash: objectHash || defaultObjectHash,
